9550d3c13f07b5058aca5ee886da7f7d2be1fadb
CodePawsDev <isripen1991@outlook.com>
2024-12-04 11:26:57 +0700

    fix: fixing test case for question-1

--- question-1.js
+++ question-1.js
@@ -4,10 +4,12 @@ function findStudentById(students, id) {
 
 // Test case
 const students = [
-  { id: 1, name: "Alice" },
-  { id: 2, name: "Bob" },
-  { id: 3, name: "Charlie" },
+  { id: 302, name: "Emily Carter" },
+  { id: 105, name: "Michael Johnson" },
+  { id: 501, name: "Sarah Wilson" },
+  { id: 207, name: "Daniel Brown" },
+  { id: 403, name: "Sophia Martinez" },
 ];
 
-console.log(findStudentById(students, 2)); // Output: { id: 2, name: 'Bob' }
+console.log(findStudentById(students, 302)); // Output: { id: 302, name: "Emily Carter" }
 console.log(findStudentById(students, 4)); // Output: null
